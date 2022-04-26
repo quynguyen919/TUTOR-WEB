52ca810e1e07a70e4a9807c7881698d6cb8a0cea
quynguyen919 <quy.ndm9039@sinhvien.hoasen.edu.vn>
2022-04-26 21:56:52 +0700

    first commen

---
+++ tutor-web/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'tutor-web-62d7b',
+    appId: '1:816452192723:web:6f520f9802556a04624726',
+    storageBucket: 'tutor-web-62d7b.appspot.com',
+    apiKey: 'AIzaSyBnFdgTLVLrZiS-yVJBxa2dwfjSQF8VcCA',
+    authDomain: 'tutor-web-62d7b.firebaseapp.com',
+    messagingSenderId: '816452192723',
+  },
+  production: true
+};

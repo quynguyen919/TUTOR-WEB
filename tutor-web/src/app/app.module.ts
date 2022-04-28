8944380b22097258ddb92716f44896cc403fb810
DucDatt <dat.hd3745@sinhvien.hoasen.edu.vn>
2022-04-28 20:46:13 +0700

    nav-bar

--- tutor-web/src/app/app.module.ts
+++ tutor-web/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from 'src/app/Shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,13 +9,18 @@ import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-// import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,7 +28,9 @@ import { FooterComponent } from './Components/footer/footer.component';
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    SharedModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
